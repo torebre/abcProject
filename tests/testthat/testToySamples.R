b56a1ba1e5e9767e513d4c16e60b9704243c4434
tore.brede@gmail.com <tore.brede@gmail.com>
2016-05-21 20:30:11 +0200

    Adding test class for toy example.

---
+++ tests/testthat/testToySamples.R
@@ -0,0 +1,37 @@
+context("Toy sample generation")
+
+test_that("No errors when creating toy samples without replicates", {
+  toy.example.configuration <-
+    smcToyExample(create.debug.variables = T)
+
+  toy.example.result <-
+    Smc(
+      toy.example.configuration,
+      max.iterations = 5,
+      alpha = 0.95,
+      number.of.particles = 10,
+      number.of.replicates = 1,
+      stop.epsilon = 0.01,
+      verbose = F
+    )
+
+})
+
+
+
+test_that("No errors when creating toy samples with replicates", {
+  toy.example.configuration <-
+    smcToyExample(create.debug.variables = T)
+
+  toy.example.result <-
+    Smc(
+      toy.example.configuration,
+      max.iterations = 5,
+      alpha = 0.95,
+      number.of.particles = 10,
+      number.of.replicates = 2,
+      stop.epsilon = 0.01,
+      verbose = F
+    )
+})
+
